3a6f0f5360b19e57f5f2dd50b0219be2c7224c3a
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-02-24 13:00:59 -0300

    Correção de  diretório

---
+++ Exercicios/Exercicio - Calculo de IMC/assets/js/main.js
@@ -0,0 +1,59 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (evento){
+    evento.preventDefault();
+    const inputPeso = evento.target.querySelector('.pesoDigitado');
+    const inputAltura = evento.target.querySelector('.alturaDigitada');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if(!peso){
+        setResultado('Peso inválido', false);
+        return;
+    }
+    if(!altura){
+        setResultado('Altura inválida', false);
+        return
+    }
+    const imc= calculaIMC(peso,altura)
+    const nivelImc = defineNivel(imc);
+    const msg =`Seu IMC é ${imc} (${nivelImc})`;
+    console.log(imc)
+
+    setResultado(msg, true )
+});
+function calculaIMC(peso, altura){
+    const imc= peso/ (altura**2)
+    return imc.toFixed(2);
+    console.log('calulado imc'+imc)
+};
+
+function criaParagrafo(){
+    const p = document.createElement('p')
+    return p;
+}
+function setResultado(msg,isValid){
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML='';
+    const p = criaParagrafo();
+    
+    if (isValid){
+        p.classList.add('paragrafo-resultado');
+    }else{
+        p.classList.add('bad');
+    }
+    
+    p.innerHTML= msg
+    resultado.appendChild(p)
+}
+function defineNivel(imc){
+    const nivel= ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+    if(imc >= 39.9) return nivel[5]
+    if(imc >= 34.9) return nivel[4]
+    if(imc >= 29.9) return nivel[3]
+    if(imc >= 24.9) return nivel[2]
+    if(imc >= 18.5) return nivel[1]
+    if(imc < 18.5) return nivel[0]
+}
---
+++ Exercicios/Exercicio - Calculo de IMC/assets/js/tentaiva-1-main.js
@@ -0,0 +1,34 @@
+function main (){
+    const form = document.querySelector('.form')
+    const container = document.querySelector('.container')
+
+    function evento(evento){
+        evento.preventDefault();
+        const peso = form.querySelector('.pesoDigitado')
+        const altura = (form.querySelector('.alturaDigitada'))
+        const resultado = container.querySelector('.resultado') 
+        const imc =  peso.value/(altura.value*altura.value).toFixed(2)
+        
+        console.log (`Seu peso é :${peso.value} e sua altura é ${altura.value} = seu imc : ${imc}`);
+
+        if (peso.value=== ''|| altura.value === '' ){
+            resultado.innerHTML = '<p class="bad">Verifique os valores inseridos e tente novamente.  </p>'
+        }else if  ( imc < 18.5 && imc>0) {
+            resultado.innerHTML = `<p class="paragrafo-resultado"> Seu IMC é: ${imc} --> Abaixo do peso</p>`
+        }else if (imc>= 18.5 && imc <24.9){
+            resultado.innerHTML = `<p class="paragrafo-resultado"> Seu IMC é: ${imc} --> Peso Normal</p>`
+        }else if(imc>= 25 && imc <29.9){
+            resultado.innerHTML = `<p class="paragrafo-resultado"> Seu IMC é: ${imc} --> Sobrepeso</p>`
+        }else if(imc>= 30 && imc <34.9){
+            resultado.innerHTML = `<p class="paragrafo-resultado"> Seu IMC é: ${imc} --> Obesidade Grau 1</p>`
+        }else if(imc>= 35 && imc <39.9){
+            resultado.innerHTML = `<p class="paragrafo-resultado"> Seu IMC é: ${imc} --> Obesidade Grau 2</p>`
+        }else if(imc> 40) {
+            resultado.innerHTML = `<p class="paragrafo-resultado"> Seu IMC é: ${imc} --> Obesidade Grau 3</p>`
+        }
+        
+
+    };
+    form.addEventListener('submit', evento)
+};
+main();
