508c39d946b153b3241370f7657f4b788e89891f
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-02-25 15:35:24 -0300

    Aula33 - desestruturação de objeto

---
+++ Aula33/script.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome:'andré',
+    sobrenome:'viado',
+    idade :25,
+    endereco:{
+        rua:'dos bobos',
+        numero: 320
+    }
+};
+
+const {nome,sobrenome} = pessoa;
+
+console.log(nome,sobrenome);
