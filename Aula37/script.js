8c90299b6305704f3214ea4edec78fdc6b6de53a
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-02-26 14:47:33 -0300

    Aula38- FOR OF

---
+++ Aula37/script.js
@@ -0,0 +1,18 @@
+const frutas = ['laranja', 'maça', 'uva', 'pera', 'mamao']
+// Exempro de uso do FOR of
+
+for(let i of frutas){
+    console.log(i)
+}
+
+// correndo um objeto com FOR OF -- nao é possivel pois o indices de um objeto nao sao ITERAVEIS
+//console.log('-----------------------------------------')
+//const cadastro = {
+//    nome:'Lorenzo',
+//    sobrenome:'Lopes',
+//    idade:25,
+//    peso:180
+//}
+//for (let x of cadastro){
+//    console.log(x)
+//}
