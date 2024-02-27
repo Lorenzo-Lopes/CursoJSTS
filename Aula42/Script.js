ebc14612676ce9581f12226aebdbd2dc7efb5db8
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-02-27 08:26:22 -0300

    atualização para ArrowFuncion

--- Aula42/Script.js
+++ Aula42/Script.js
@@ -1,8 +1,18 @@
+/*
 function formatoIMG(altura, largura){
-    if (altura<largura) console.log('a imagem esta em modo paisagem.')
-    if (largura<altura) console.log(' a imagem esta em modo retrado')
+    if (altura<largura) console.log(altura<largura)
+    if (largura<altura) console.log(altura<largura)
+    
 }
+
 let altura = 10800
 let largura = 1920
 
 formatoIMG(altura,largura);
+*/
+const ePaisagem = (largura, altura)=> largura>=altura
+
+let altura = 1080
+let largura = 1920
+
+console.log(ePaisagem(largura,altura))
