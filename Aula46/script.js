35e16c5aae6cafd9ab887edd9938f9c5595cb09c
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-02-27 23:18:41 -0300

    correção

--- Aula46/script.js
+++ Aula46/script.js
@@ -12,6 +12,8 @@ setTimeout(function(){
     clearInterval(timer);
 },9000)
 
-setTimeout(function(){
-    console.log('Ja acabou, pode ir embora.')
-},10000)
+//setTimeout(function(){
+//    console.log('Ja acabou, pode ir embora.')
+//},10000)
+
+//cronometo.toLocaleTimeString('pt-BR',{  hour12: false});
