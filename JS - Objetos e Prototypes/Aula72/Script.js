1789abbdce3a573b9bc07df7198ff5ea60dff31d
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-03-02 18:51:58 -0300

    Aula72 - Get e Set em Funções Construtoras

---
+++ JS - Objetos e Prototypes/Aula72/Script.js
@@ -0,0 +1,40 @@
+//defineProperty -> Getter e Setter
+function Produto(nome, preco,estoque){
+    this.nome;
+    this.preco;
+
+    let estoqueP = estoque;
+    Object.defineProperty(this,'estoque',{
+        enumerable:false,// permite que o propriedade 'estoque' seja exibida
+        configurable: true,// os parametros acima pode ser redefinidos em outro bloco de codigo ### Se falso tambem bloqueia a exclução da propriedade
+        get: function(){
+            return estoqueP
+        },
+        set: function(valor){
+            if (typeof valor !== 'number'){
+                throw new TypeError('Ta errado')
+            }
+            estoqueP = valor
+        }
+    });
+    }
+
+const p1 = new Produto('Camiseta',20,3)
+p1.estoque = 555
+console.log(p1.estoque)
+
+function criaProduto (nome){
+    return{
+        get nome(){
+            return nome;
+        },
+        set nome (valor){
+            nome= valor 
+        }
+    }
+}
+const p2 = criaProduto('notebook');
+
+console.log (p2.nome)
+p2.nome='Joao'
+console.log (p2.nome)
