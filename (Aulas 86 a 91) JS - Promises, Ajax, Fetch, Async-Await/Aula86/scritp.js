5b7c07559429b8a4d4db6d15de9e5de54935e8a8
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-03-05 21:37:12 -0300

    Aula86- Promises

---
+++ (Aulas 86 a 91) JS - Promises, Ajax, Fetch, Async-Await/Aula86/scritp.js
@@ -0,0 +1,38 @@
+function rand(){
+    const min = 1* 500
+    const max = 3* 500
+    return Math.floor(Math.random()* (max-min)+min);
+}
+function conectbd(msg,tempo){
+    return new Promise((resolve,reject)=>{
+        if (typeof msg !=='string') reject(new TypeError('erro'));
+
+        setTimeout(()=>{
+        resolve(msg)
+
+    },tempo)
+
+    })
+    
+
+    
+}
+conectbd('Iniciado conexão  com o banco.',rand()).then(resposta =>{
+    console.log(resposta);
+    return conectbd('Buscando dados...' ,rand()).then(resposta=>{
+        console.log(resposta)
+        return conectbd('Exebi dado na tela', rand()).then(resposta =>{
+        console.log(resposta)
+        });
+    }).catch(e=>{
+        console.log('ERRO',e)
+    })
+})
+
+for (let i =0; i<100;i++){
+    setTimeout(()=>{
+        console.log("Carregando")
+
+    },rand())
+
+}
