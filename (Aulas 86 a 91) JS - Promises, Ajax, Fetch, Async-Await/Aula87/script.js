2ff2797e09fbc10e5ad0a2e9b6c6d1333ca90398
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-03-05 22:08:11 -0300

    Aula87 - Métodos para Premisses

---
+++ (Aulas 86 a 91) JS - Promises, Ajax, Fetch, Async-Await/Aula87/script.js
@@ -0,0 +1,53 @@
+// Promise.all
+// Promise.race
+// Promise.resolve
+// Promise.reject
+
+function rand(min, max){
+     min *= 1000
+    max *= 1000
+    return Math.floor(Math.random()* (max-min)+min);
+}
+function espera(msg,tempo){
+    return new Promise((resolve,reject)=>{
+        if (typeof msg !=='string'){
+         reject(new TypeError('erro'));
+         return
+        }
+        setTimeout(()=>{
+        resolve(msg)
+        },tempo);
+    });
+}
+/*
+const promises = [
+    //'primeiro',
+    espera('promise 1',rand(1,3)),
+    espera('promise 2', rand(1,3)),
+    espera('promise 3', rand(1,3)),
+    //espera(10,1000),
+    //'ultimo'    
+];
+//.all resolve todas as promises 
+//.race retorna a primeira promise resolvida.
+
+/*
+Promise.race(promises).then(function(valor){
+    console.log(valor)
+})
+.catch(function(erro){
+    console.log(erro);
+})*/
+
+function baixarPagina() {
+    const emCache = false
+    
+    if(emCache){
+        return Promise.reject/* .resolve*/('Monstra pagina')
+    }else{
+        return espera('Baixei a pagina',3000)
+    }
+}
+baixarPagina().then(dadosPg =>{
+    console.log(dadosPg)
+}).catch(e => console.log('ERRO',e))
