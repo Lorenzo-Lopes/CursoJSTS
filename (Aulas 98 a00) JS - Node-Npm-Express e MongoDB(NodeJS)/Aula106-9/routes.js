055bac9f1923b9992d1b35dc89a36b9b9a9beb5d
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-03-14 14:42:48 -0300

    Aula106 - routes

---
+++ (Aulas 98 a00) JS - Node-Npm-Express e MongoDB(NodeJS)/Aula106-9/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const route  = express.Router()
+const homeControler = require('./controllers/homeController')
+const contatoControler = require('./controllers/contatoController')
+route.get('/',homeControler.paginaInicial)
+route.post('/', homeControler.trataPost)
+
+route.get('/contato', contatoControler.paginaInicial)
+
+module.exports = route
