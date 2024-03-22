4e018ad4a3ff643bf649d254bb75bea992bc5e53
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-03-21 23:51:39 -0300

    AluraMidi

---
+++ AluraMidi/main.js
@@ -0,0 +1,38 @@
+// Pom ----------------------------------------
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+let i =0;
+
+while(i <listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+        
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    contador++;
+   
+}
+
+for(contador=0; contador<listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+        
+    tecla.onclick(space) = function () {
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+        tecla.classList.remove('ativa');
+    }
+}
+    
